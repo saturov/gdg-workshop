43ee1b6c7e311300dce0b3a0c3db8d40649654da
Eugene Saturov <saturov@surfstudio.ru>
2019-11-10 21:05:16 +0300

    Location Permission screen UI

--- lib/ui/screen/location_permission/location_permission_widget.dart
+++ lib/ui/screen/location_permission/location_permission_widget.dart
@@ -11,6 +11,45 @@ class LocationPermissionScreen extends StatefulWidget {
 class LocationPermissionState extends State {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Мы должны получить\nдоступ к геолокации',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 20.0,
+                  ),
+                  child: MaterialButton(
+                    color: Colors.blue,
+                    onPressed: () => {
+                      //todo ask for the location permission
+                    },
+                    child: Text(
+                      'Замечательно',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
