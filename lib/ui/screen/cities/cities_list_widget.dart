5392ce9f6bcdef6795b7924055c5d519a4c43dd4
Eugene Saturov <saturov@surfstudio.ru>
2019-11-11 21:15:19 +0300

    Json loading and decoding

--- lib/ui/screen/cities/cities_list_widget.dart
+++ lib/ui/screen/cities/cities_list_widget.dart
@@ -1,17 +1,25 @@
+import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CitiesListScreen extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return CitiesListScreenState();
   }
-
 }
 
 class CitiesListScreenState extends State {
+  String rawJson = "";
+
+  @override
+  void initState() {
+    extractJson().then((value) {
+      rawJson = value;
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +28,19 @@ class CitiesListScreenState extends State {
         title: Text('GDG Everywhere'),
         automaticallyImplyLeading: false,
       ),
+      body: Text(
+        rawJson,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
     );
   }
 
+  Future<String> extractJson() async {
+    String data = await DefaultAssetBundle.of(context)
+        .loadString("res/assets/cities.json");
+    final jsonResult = json.decode(data);
+    return jsonResult;
+  }
 }
