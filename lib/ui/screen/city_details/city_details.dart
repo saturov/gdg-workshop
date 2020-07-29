14b95fa33cb3069dad3f00bf178fd2eede0a281f
zaytsev <zaytsev@surfstudio.ru>
2020-07-29 14:05:13 +0300

    add button to know count of community  in city

--- lib/ui/screen/city_details/city_details.dart
+++ lib/ui/screen/city_details/city_details.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class CityDetailsScreen extends StatelessWidget {
@@ -34,16 +36,80 @@ class CityDetailsScreen extends StatelessWidget {
             ),
           ),
         ],
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            description,
-            style: TextStyle(
-              fontSize: 16.0,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Объем коммъюнити: "),
+                    _LoadCountButton()
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  description,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class _LoadCountButton extends StatefulWidget {
+  @override
+  __LoadCountButtonState createState() => __LoadCountButtonState();
+}
+
+class __LoadCountButtonState extends State<_LoadCountButton> {
+  bool _loading = false;
+  int _communityCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 100,
+      child: _loading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : _communityCount != null
+              ? Center(
+                  child: Text("$_communityCount"),
+                )
+              : FlatButton(
+                  onPressed: _loadData,
+                  child: Text("Узнать"),
+                ),
+    );
+  }
+
+  void _loadData() async {
+    setState(() {
+      _loading = true;
+    });
+
+    var res = await Future.delayed(
+      Duration(seconds: 2),
+      () => (math.Random().nextDouble() * 100).toInt(),
+    );
+
+    setState(() {
+      _communityCount = res;
+      _loading = false;
+    });
   }
 }
